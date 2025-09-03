9354fd7aa36e1f2e5a6d3e9d32cb9afbfe77b760
Diana Sosnovskaya <progdidi@gmail.com>
2025-09-03 23:36:54 +0300

    feat: feat Footer component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Footer/>
+      <Header/>
+        <main className="main">
+          
+        </main>
+      <Footer/>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,8 @@
+import './footer.scss';
 import SocialList from "../SocialList/SocialList";
+
+//images
+import logo from './images/logo.svg';
 
 const Footer = () => {
     return ( 
@@ -6,8 +10,45 @@ const Footer = () => {
             <div className="container">
                 <div className="footer__inner">
                     <div className="footer-info">
+                        <img src={logo} alt="" className="footer-logo" />
                         <SocialList/>
                     </div>
+
+                    <ul className="footer-list">
+                        <h6 className="footer-list__title">Our Destinations</h6>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Canada</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Alaksa</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">France</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Iceland</a></li>
+                    </ul>
+
+                    <ul className="footer-list">
+                        <h6 className="footer-list__title">Our Activities</h6>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Northern Lights</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Cruising & sailing</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Multi-activities</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Kayaing</a></li>
+                    </ul>
+
+                    <ul className="footer-list">
+                        <h6 className="footer-list__title">Travel Blogs</h6>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Bali Travel Guide</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Sri Lanks Travel Guide</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Peru Travel Guide</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Bali Travel Guide</a></li>
+                    </ul>
+
+                    <ul className="footer-list">
+                        <h6 className="footer-list__title">About Us</h6>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Our Story</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Work with us</a></li>
+                    </ul>
+
+                    <ul className="footer-list">
+                        <h6 className="footer-list__title">Contact Us</h6>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Our Story</a></li>
+                        <li className="footer-list__item"><a href="" className="footer-list__item-link">Work with us</a></li>
+                    </ul>
                 </div>
             </div>
         </footer>
